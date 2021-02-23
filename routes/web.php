ae87890e77287b45cc76b66f3d7364b6e8ae53d3
riyanlasso <riyanlasso@gmail.com>
2021-02-23 13:41:04 +0700

    PRAKTIKUM 1

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    echo 'SELAMAT DATANG';
+});
+Route::get('/about', function () {
+    echo 'NIM : 1941720203 <br><br>';
+    echo 'NAMA : FITRANZA AKBAR SARRAZIN';
+});
+Route::get('/articles/{id}', function ($id) {
+    echo 'Halaman Artikel dengan ID '.$id ;
+});
+
+?>
+
